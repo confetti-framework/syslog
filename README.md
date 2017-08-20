deb3483f0e4ec3d641505367f03a0b28ae2fab08
Péter Szakszon <szp0530@gmail.com>
2017-08-20 21:44:58 +0200

    update readme

--- README.md
+++ README.md
@@ -21,6 +21,10 @@ func main() {
 	buf := &bytes.Buffer{}
 	l := syslog.NewLogger(buf, syslog.USER, "hostname", "appName", "procid")
 
+	// without structured data
+	l.Log(syslog.INFO, "ImageUploaded", nil, "image uploaded by %s: %s", "username", "image.jpg")
+
+	// with structured data
 	sd := syslog.StructuredData{}
 	sd.Element("id1").Set("par1", "val1")
 	l.Log(syslog.ERR, "LoginFailed", sd, "login failed: %s", "username")
@@ -28,6 +32,7 @@ func main() {
 	fmt.Print(buf.String())
 
 	// Output is similar to this:
+	// <14>1 2017-08-15T23:13:15.335+02:00 hostname appName procid ImageUploaded - image uploaded by username: image.jpg
 	// <11>1 2017-08-15T23:13:15.335+02:00 hostname appName procid LoginFailed [id1 par1="val1"] login failed: username
 }	
 ```
--- example_new_logger_test.go
+++ example_new_logger_test.go
@@ -14,6 +14,10 @@ func ExampleNewLogger() {
 	buf := &bytes.Buffer{}
 	l := syslog.NewLogger(buf, syslog.USER, "hostname", "appName", "procid")
 
+	// without structured data
+	l.Log(syslog.INFO, "ImageUploaded", nil, "image uploaded by %s: %s", "username", "image.jpg")
+
+	// with structured data
 	sd := syslog.StructuredData{}
 	sd.Element("id1").Set("par1", "val1")
 	l.Log(syslog.ERR, "LoginFailed", sd, "login failed: %s", "username")
@@ -21,5 +25,6 @@ func ExampleNewLogger() {
 	fmt.Print(buf.String())
 
 	// Output is similar to this:
+	// <14>1 2017-08-15T23:13:15.335+02:00 hostname appName procid ImageUploaded - image uploaded by username: image.jpg
 	// <11>1 2017-08-15T23:13:15.335+02:00 hostname appName procid LoginFailed [id1 par1="val1"] login failed: username
 }
